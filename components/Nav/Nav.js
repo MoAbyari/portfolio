1947a82ef4353c214f87c696455ff2926c958bd5
MoAbyari <mo.abyari88@gmail.com>
2021-09-21 17:51:03 +1000

    Added Font

--- components/Nav/Nav.js
+++ components/Nav/Nav.js
@@ -10,13 +10,16 @@ const Nav = () => {
     const scrollToProjects = () => {
         document.getElementById("projects").scrollIntoView({ behavior: 'smooth', block: 'center' });
     }
+    const openResume = () => {
+        window.open('/Mo Abyari.pdf', "_blank");
+    }
     return (
         <header className= {style.header}>
             <nav className= {style.nav}>
                 <ul className= {style.navlist}>   
                     <li><button onClick={scrollToAbout}> <IoIosPerson className={style.icon1} />  About</button></li>
                     <li><button onClick={scrollToProjects}> <AiOutlineAppstore size={15} />  Projects</button></li>
-                    <li><button> <IoIosDocument className={style.icon1} />  Resume</button></li>
+                    <li><button onClick={openResume}> <IoIosDocument className={style.icon1} />  Resume</button></li>
                 </ul>
             </nav>
         </header>
--- pages/projects/index.js
+++ pages/projects/index.js
@@ -9,4 +9,4 @@ const Projects = () => {
     )
 }
 
-export default Projects;
+export default Projects; 
